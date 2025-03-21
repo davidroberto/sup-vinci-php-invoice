6b3644bc3c129f4c2623d38cc9b1314f509f0458
David Robert <contact@david-robert.fr>
2025-03-21 16:32:58 +0100

    entity invoice

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -10,7 +10,18 @@ class InvoiceController extends AbstractController
 {
     #[Route('/invoice', name: 'get_invoice', methods: ['GET'])]
     public function getInvoice() {
-       return $this->render('get-invoice.html.twig');
+
+        $invoiceFakeFromDb = [
+            "id" => 1,
+            "title" => "Invoice 1",
+            "description" => "Invoice description 1",
+            "price" => 200,
+            "status" => "pending",
+        ];
+
+       return $this->render('get-invoice.html.twig', [
+           'invoice' => $invoiceFakeFromDb,
+       ]);
     }
 
 }
---
+++ src/Entity/Invoice.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InvoiceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InvoiceRepository::class)]
+class Invoice
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
